d9df6ecc5260a3a6a74798685473dbd94b91cc61
gbmanikandan33 <gbmani901@gmail.com>
2021-02-05 18:30:52 -0800

    programing code

---
+++ src/org/demo/FirstProgram.java
@@ -0,0 +1,25 @@
+package org.demo;
+
+public class FirstProgram 
+{
+private void javaCource() {
+	//  method1
+	System.out.println("java cource is started");
+
+}
+private void oracleCource() {
+	// TODO Auto-generated method stub
+
+System.out.println("oracle cource is started");
+}
+
+public static void main(String[] args) {
+	
+//ClassName objname=new ClassName();
+	FirstProgram j=new FirstProgram();
+	j.javaCource();
+	j.oracleCource();
+	
+}
+
+}
